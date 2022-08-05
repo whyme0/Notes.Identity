18955536b7c77ebe4efa6fcba6b670213bdede7e
whyme0 <sparkjetstudiost@gmail.com>
2022-08-05 20:04:41 +0300

    register identityserver in DI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
+using IdentityServer4.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register IdentityServer4
+builder.Services.AddIdentityServer()
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddInMemoryClients(new List<Client>())
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
+
+app.UseRouting();
+
+// IdentityServer4 (OAuth 2.0 & OpenID Connect)
+app.UseIdentityServer();
 
 app.Run();
