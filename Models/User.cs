c8edbdbf0c734375b856c081b5d0020416e0b106
whyme0 <sparkjetstudiost@gmail.com>
2022-08-06 11:11:28 +0300

    create custom identityuser

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Notes.Identity.Models
+{
+    public class User : IdentityUser
+    {
+        public readonly DateTime RegistrationDate = DateTime.Now;
+    }
+}
