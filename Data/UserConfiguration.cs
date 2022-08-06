d55acb9508788245af7442eaf3e7c641eda2235a
whyme0 <sparkjetstudiost@gmail.com>
2022-08-06 11:12:03 +0300

    create configuration for user model

---
+++ Data/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Identity.Models;
+
+namespace Notes.Identity.Data
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.HasIndex(u => u.Id).IsUnique();
+        }
+    }
+}
