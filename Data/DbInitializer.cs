db995832d31512454aebf470a87f0dfc44787609
whyme0 <sparkjetstudiost@gmail.com>
2022-08-06 11:12:28 +0300

    create db initializer

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Identity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AuthDbContext dbContext)
+        {
+            dbContext.Database.EnsureCreated();
+        }
+    }
+}
